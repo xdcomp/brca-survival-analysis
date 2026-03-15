f70b1d251310408edf93b3af50cee1291fdfb613
xdwctd100111 <delcidxav@gmail.com>
2026-03-14 21:09:00 -0400

    composite event function, used function on cleaned data to get new tumor_death column in clin_cleaned

--- proj.py
+++ proj.py
@@ -1,4 +1,4 @@
-#immune infiltration signatures predict survival in breat cancer - we will look at the expression of immune genes in mrna data, and survival times (OS_MONTHS and OS_STATUS) in clinical data
+#immune infiltration signatures predict survival in breat cancer - we will look at the expression of immune genes in mrna data, and survival times (DSS_MONTHS and OS_STATUS) in clinical data
 #immune infiltration just means expression of immune genes
 
 
@@ -7,85 +7,75 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from lifelines import KaplanMeierFitter
 
-##load data from cBioPortal
+#==================== LOAD DATA (GATHERED FROM CBIOPORTAL) ==========================
 
 #expression data
-#exp = pd.read_csv(
-#    r"C:/Users/xavr/Documents/brca/data/data_mrna_seq_v2_rsem.txt",
-#    sep="\t",
-#    comment="#"
-#)
+exp = pd.read_csv(
+    r"C:/Users/xavr/Documents/brca/data/data_mrna_seq_v2_rsem.txt",
+    sep="\t",
+    comment="#"
+)
 
 #clinical data 
-#clin = pd.read_csv(
-#    r"C:/Users/xavr/Documents/brca/data/data_clinical_patient.txt",
-#    sep="\t",
-#    comment="#"
-#)
+clin = pd.read_csv(
+    r"C:/Users/xavr/Documents/brca/data/data_clinical_patient.txt",
+    sep="\t",
+    comment="#"
+)
 
 
 #for mac
 
-exp = pd.read_csv(r"/Users/user/Desktop/Bioinformatics/brca/data_mrna_seq_v2_rsem.txt",
-    sep="\t",
-    comment="#"
-)
-clin = pd.read_csv(r"/Users/user/Desktop/Bioinformatics/brca/data_clinicaL_patient.txt",
-    sep="\t",
-    comment="#"
-)
-print(clin["OS_MONTHS"])
+#exp = pd.read_csv(r"/Users/user/Desktop/Bioinformatics/brca/data_mrna_seq_v2_rsem.txt",
+#    sep="\t",
+#    comment="#"
+#)
+#clin = pd.read_csv(r"/Users/user/Desktop/Bioinformatics/brca/data_clinicaL_patient.txt",
+#    sep="\t",
+#    comment="#"
+#)
 
-#extract survival data
+#==================== EXTRACT SURVIVAL DATA ==========================
 
-#first check for missing values from our survival data (OS_MONTHS, OS_STATUS, DSS_STATUS)
+#we will use 3 survival variables from the patient data for survival analysis
+    #1. DSS_MONTHS, records time from initial diagnosis to time of cancer-specific death
+    #2. OS_STATUS, records patient death due to cancer and potentially non-cancer specific causes
+    #3. DSS_STATUS, records cancer-related death, but is ambiguous as it gives a dead or alive status for many patients. This will be used together with OS_STATUS to confirm cancer-related death 
 
-#check 
+#first check for missing values from our survival data (DSS_MONTHS, OS_STATUS, DSS_STATUS)
 print("CHECK FOR MISSING VALUES")
 
-print(f"OS_MONTHS missing: {clin['OS_MONTHS'].isna().sum()}")
+print(f"DSS_MONTHS missing: {clin['DSS_MONTHS'].isna().sum()}")
 print(f"OS_STATUS missing: {clin['OS_STATUS'].isna().sum()}")
-
-
-print(f"DSS_STATUS missing: {clin['DSS_STATUS'].isna().sum()}")
+print(f"DSS_STATUS missing: {clin['DSS_STATUS'].isna().sum()}") #MISSING 20 PATIENTS
 
 #using .dropna, remove the 20 out of 1084 patients missing DSS_STATUS values
-clin_cleaned = clin.dropna(subset = ["DSS_STATUS"])
+clin_cleaned = clin.dropna(subset = ["DSS_STATUS"]).copy()
 
 
-#months from initial diagnosis to recorded death
-months = clin_cleaned["OS_MONTHS"]
-
-#extract dead or alive survival status (includes non-cancer related deaths)
-os_stat = clin_cleaned["OS_STATUS"] #0 = ALIVE, 1 = DEAD
-#simplify OS_STAT status to just the 0s and 1s
-os_stat_val = clin_cleaned["OS_STATUS"].str.split(':').str[0].astype(int)
-
-#patient's dead or alive status (includes only cancer-related deaths, but needs to be tied to os_stat due for ambiguous "dead/alive tumor free" patient status)
-dss_stat = clin_cleaned["DSS_STATUS"]
-#simplify DSS_STATUS to 0s and 1s
-dss_stat_val = dss_stat.str.split(':').str[0].astype(int)
+#months from initial diagnosis to cancer-related death
+months = clin_cleaned["DSS_MONTHS"]
 
 
-
-#composite event variable function
-#this is the variable we will use to confirm death by cancer
-
+#composite event variable function, where event is cancer death
 def cancer_death(row):
+    os_s = int(str(row["OS_STATUS"]).split(":")[0])
+    dss = int(str(row["OS_STATUS"]).split(":")[0])
+
+    #if os_s = 1("DECEASED") and dss = 1 ("DEAD WITH TUMOR")
+    if os_s == 1 and dss == 1:
+        return 1
+    else:
+        return 0
 
 
-    if os_stat_val and dss_stat_val == 0:
-        return 0 #ALIVE, cancer free
-    elif os_stat_val == 0 and dss_stat_val == 1:
-        return 0 #alive, cancer free
-    elif os_stat_val and dss_stat_val == 1:
-        return 1 #dead, had cancer
-    #check data to make sure no rows where OS_STATUS= 0 and DSS_STATS= 1 (contradiction, alive and dead with tumor)
+#create new column "tumor_death" in cleaned dataset using the function
+clin_cleaned["tumor_death"] = clin_cleaned.apply(cancer_death, axis=1)
+#print(clin_cleaned["tumor_death"].value_counts(dropna=False))
 
-#os_stat_val
-#0:LIVINg 1:DEAD WITH TUMOR
 
-#dsstat_val0:ALIVE OR DEAD TUMOR FREE1:DECEASED
+
+
 
 
 
